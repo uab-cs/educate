c21fd741e2c264e2e9967af40e9f7d4d179b6645
Chris Rocco <chris.rocco7@gmail.com>
2017-08-28 19:16:30 -0500

    Boilerplate: view, view model, AngularJS, MathJax, deploy script, template

---
+++ README.md
@@ -0,0 +1,12 @@
+POLYNOMIAL ROOTS
+========
+
+### Overview
+Educational software that teaches you how to factor polynomial roots. 
+CS 420 Software Engineering project.
+
+### Goals
+1. **Version Control**: Employ traditional branching strategies to working in parallel and avoid conflicts.
+2. **Logistics**: The hardest part of any project is often working with people. We will have regularly scheduled meetings at 10am on Monday’s to coordinate our efforts. We will perform general project assessment
+3. **Modular Design**: Divide the application into independent modules which can be developed independently. Collaborators will depend on each module having its own published API.
+4. **Disciplined Development Strategy**: We will practice a subtype of agile development which most resembles extreme programming. We will program in pairs, keeping one partner free to keep the project on track, and leave the other entirely focused on the code.
---
+++ app/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Chris Rocco on 8/28/2017.
+ */
+
+let app = angular.module("app", ['ngJaxBind']);
+
+app.controller("AppController", AppController);
+
+function AppController($scope){
+    /* data model */
+    $scope.expression = "$$ 4x^3 - 10y^2 + 13z + 25 $$";
+    $scope.result = false;
+
+    /* functions */
+    $scope.handleGo = handleGo;
+    $scope.handleTryAgain = handleTryAgain;
+
+    /* constructor */
+    function init(){
+        MathJax.Hub.Config({
+            tex2jax: {
+                inlineMath: [['$','$'], ['\\(','\\)']],
+                processEscapes: true
+            }
+        });
+    }
+
+    /*=====================================================*/
+    /*================== Business Logic ==================*/
+    /*=====================================================*/
+
+    function handleGo(){
+        $("#portfolio-ajax-loader").show();
+        setTimeout(function(){
+            $scope.result = "$$ (x + 5)(y - 3)(z + 12) $$";
+            $scope.$apply();
+            $("#portfolio-ajax-loader").hide();
+        }, 2000);
+        // swal("Coming Soon!", "This page is under development", "info");
+    }
+
+    function handleTryAgain(){
+        $scope.result = false;
+    }
+
+    init();
+}

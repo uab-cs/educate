b808e515850ea5db42d9a11c7b4a3499932e3826
Chris Rocco <chris.rocco7@gmail.com>
2017-08-30 00:10:58 -0500

    added table to display parsing output

--- app/app.js
+++ app/app.js
@@ -6,27 +6,35 @@ let app = angular.module("app", ['ngJaxBind']);
 
 app.controller("AppController", AppController);
 
-AppController.$inject = ['$scope'];
-function AppController($scope){
+AppController.$inject = ['$scope', 'polyService'];
+function AppController($scope, polyService){
     /* data model */
-    $scope.expression = "4x^3 - 10y^2 + 13z + 25";
+    $scope.expression = "30x^5 - 166x^4 - 542x^3 + 2838x^2 + 1520x - 800";
     $scope.result = false;
 
     /* functions */
     $scope.handleGo = handleGo;
     $scope.handleTryAgain = handleTryAgain;
+    $scope.parse = parse;
 
     /* constructor */
     function init(){
         /* for debugging purposes only*/
         window.scope = $scope;
         /* end */
+
+        /* setup mathjax */
         MathJax.Hub.Config({
             tex2jax: {
                 inlineMath: [['$','$'], ['\\(','\\)']],
                 processEscapes: true
             }
         });
+        // end
+
+        /* trigger parsing for the example */
+        parse($scope.expression);
+        // end
     }
 
     /*=====================================================*/
@@ -36,7 +44,7 @@ function AppController($scope){
     function handleGo(){
         $scope.loading = true;
         setTimeout(function(){
-            $scope.result = "4x^3 - 10y^2 + 13z + 25";
+            $scope.result = "4x^3 - 10x^2 + 13x + 25";
             $scope.loading = false;
             $scope.$apply();
         }, 2000);
@@ -44,6 +52,13 @@ function AppController($scope){
 
     function handleTryAgain(){
         $scope.result = false;
+    }
+
+    function parse(expression){
+        let p  = polyService.parse(expression);
+        console.log(expression, p);
+        if(p === false) return;
+        $scope.polynomial = p;
     }
 
     init();

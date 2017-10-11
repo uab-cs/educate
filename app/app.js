33c4ab177be8c379330e6b5991322e075cec6201
Chris Rocco <chris.rocco7@gmail.com>
2017-10-10 19:59:19 -0500

    integrated with polymath library

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@
  * Created by Chris Rocco on 8/28/2017.
  */
 
-let app = angular.module("app", ['ngJaxBind']);
+var app = angular.module("app", ['ngJaxBind']);
 
 app.controller("AppController", function($scope){
-    $scope.expression = "15x^3 + 14x^2 - 3x - 2";
+    $scope.expression = "12x^3-41x^2-38x+40";
+    // $scope.expression = "15x^3 + 14x^2 - 3x - 2";
+    $scope.result = false;
 
     /* constructor */
     function init(){
@@ -25,9 +27,45 @@ app.controller("AppController", function($scope){
         });
     }
 
-    $scope.go = function(){
+    $scope.go = function(polynomialExpression){
+        var polynomial = window.polymath.parse(polynomialExpression);
+        var signs = polymath.ruleOfSigns(polynomial);
+        var zeros = polymath.rationalZeroTest(polynomial);
+        console.log(polynomial);
+        console.log(signs);
+        console.log(zeros);
+        renderZeros(zeros.actual_zeros);
+    };
+
+    $scope.showSteps = function(){
         swal("Coming Soon!");
     };
+
+    function renderZeros(zeros){
+        var latex = [];
+        zeros.forEach(function(zero){
+            latex.push( toLatex(zero) );
+        });
+        $scope.result = latex.join("");
+    }
+
+    /**
+     *
+     * @param root Fraction
+     * @returns {string}
+     */
+    function toLatex(root){
+        var num = root.n;
+        var den = root.d;
+        var r = "\\frac{"+num+"}{"+den+"}";
+        if(num === den)
+            r = num;
+        var sign = "+";
+        if(root < 0)
+            sign = "-";
+        return "(x"+sign+r+")";
+    }
+
 
     function renderFunction(expression, selector){
         functionPlot({
